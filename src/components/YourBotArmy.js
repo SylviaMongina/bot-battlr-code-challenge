dcfdd60bc878f41cfbe4358c24c3f2fb7fe8c8aa
SylviaMonginaBosire <sylvia.bosire@student.moringaschool.com>
2023-01-22 12:09:19 +0300

    coding

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -10,7 +10,7 @@ function YourBotArmy({ botArmy, releaseBot, dischargeBot }) {
         <div className="row bot-army-row">
           <h3>{botArmy.map((bot) => (
             <BotCard bot={bot} handleBot={releaseBot} handleDelete={dischargeBot} />))}</h3>
-          Your Bot Army
+         Click on a Bot on the bot collection to enlist it to your army.
         </div>
       </div>
     </div>
